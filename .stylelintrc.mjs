5515c2da55d4f10f673af3feced501221703a1c2
ics-ikeda <ikeda@ics-web.jp>
2025-05-20 09:28:37 +0900

    @charset "UTF-8"; を消す・Prettier導入

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,17 +1,14 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: [
-    "stylelint-config-standard-scss",
-    "stylelint-config-recess-order",
-  ],
+  extends: ["stylelint-config-standard-scss", "stylelint-config-recess-order"],
   rules: {
     "scss/at-mixin-pattern": null,
     "selector-class-pattern": null,
     "property-no-vendor-prefix": [
       true,
       {
-        ignoreProperties: ["user-select"]
-      }
-    ]
-  }
-}
+        ignoreProperties: ["user-select"],
+      },
+    ],
+  },
+};
